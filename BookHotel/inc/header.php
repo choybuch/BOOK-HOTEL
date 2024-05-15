209ad8fdcd9ab541349e52e457f5a5e0f81c4f5a
Elijah Bustamante <124417882+choybuch@users.noreply.github.com>
2024-05-16 02:38:17 +0800

    Add files via upload

--- BookHotel/inc/header.php
+++ BookHotel/inc/header.php
@@ -1,18 +1,17 @@
-<?php 
-    require('admin/inc/db_config.php');
-    require('admin/inc/essentials.php');
+<style>
+.dropdown { 
+    position: absolute; 
+    right: 0; 
+}
+</style>
 
-    $contact_q = "SELECT * FROM `contact_details` WHERE `sr_no`=?";
-     $values = [1];
-     $contact_r = mysqli_fetch_assoc(select($contact_q,$values,'i'));
-?>
 
 <nav id="nav-bar" class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
-    <div class="container-fluid">
-        <a class="navbar-brand me-5 fw-bold fs-2 h-font" href="index.php">BookHotel</a>
-            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+        <a class="navbar-brand me-5 fw-bold fs-2 h-font" href="index.php"><?php echo $settings_r['site_title'] ?></a>
+        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
@@ -32,21 +31,47 @@
                 </li>
             </ul>
             <div class="d-flex">
-                <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
-                    Login
-                </button>
-                <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
-                    Register
-                </button>
+                <?php 
+                    if(isset($_SESSION['login']) && $_SESSION['login']==true)
+                    {
+                        $path = USERS_IMG_PATH;
+                        echo<<<data
+                        <div class="btn-group ">
+                            <button type="button" class="btn btn-outline-dark shadow-none dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                                <img src="$path$_SESSION[uPic]" style="width: 25px; height: 25px" class="me-1">
+                                $_SESSION[uName]
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-lg-end">
+                                <li><a button class="dropdown-item" href="profile.php">Profile</a></li>
+                                <li><a button class="dropdown-item" href="bookings.php">Bookings</a></li>
+                                <li><a button class="dropdown-item" href="logout.php">Logout</a></li>
+                            </ul>
+                        </div>
+                        data;
+                    }
+                    else{
+                        echo<<<data
+                        <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+                            Login
+                        </button>
+                        <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+                            Register
+                        </button> 
+                        data;
+                    }
+                ?>
+                
             </div>
         </div>
     </div>
 </nav>
+
+
  
 <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-        <form action="">
+        <form id="login-form">
             <div class="modal-header">
                 <h5 class="modal-title d-flex align-items-center">
                 <i class="bi bi-person-circle fs-3 me-2"></i> User Login
@@ -55,16 +80,18 @@
             </div>
             <div class="modal-body">
                 <div class="mb-3">
-                    <label class="form-label">Email address</label>
-                    <input type="email" class="form-control shadow-none">
+                    <label class="form-label">Email / Mobile</label>
+                    <input type="text" name="email_mob" required class="form-control shadow-none">
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Password</label>
-                    <input type="password" class="form-control shadow-none">
+                    <input type="password" name="pass" required class="form-control shadow-none">
                 </div>
                 <div class="d-flex align-items-center justify-content-between mb-2">
                     <button type="submit" class="btn btn-dark shadow-none">LOGIN</button>
-                    <a href="javascript: void(0)" class="text-scondary text-decoration-none">Forgot Password?</a>
+                    <!--<button type="button" class="btn text-secondary text-decoration-none shadow-none p-0" data-bs-toggle="modal" data-bs-target="#forgotModal" data-bs-dismiss="modal">
+                        Forgot Password
+                    </button> -->
                 </div>
             </div>
         </form>
@@ -76,7 +103,7 @@
 <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
-        <form action="">
+        <form id="registration-form">
             <div class="modal-header">
                 <h5 class="modal-title d-flex align-items-center">
                 <i class="bi bi-person-lines-fill fs-3 me-2"></i>
@@ -93,39 +120,39 @@
                 <div class="row">
                     <div class="col-md-6 ps-0 mb-3">
                         <label class="form-label">Name</label>
-                        <input type="text" class="form-control shadow-none">
+                        <input name="name" type="text" class="form-control shadow-none" required>
                     </div>
                     <div class="col-md-6 p-0 mb-3">
                         <label class="form-label">Email</label>
-                        <input type="email" class="form-control shadow-none">
+                        <input name="email" type="email" class="form-control shadow-none" required>
                     </div>
                     <div class="col-md-6 ps-0 mb-3">
                         <label class="form-label">Phone Number</label>
-                        <input type="number" class="form-control shadow-none">
+                        <input name="phonenum" type="number" class="form-control shadow-none" required>
                     </div>
                     <div class="col-md-6 p-0 mb-3">
                         <label class="form-label">Picture</label>
-                        <input type="file" class="form-control shadow-none">
+                        <input name="profile" type="file" accept=".jpg, .jpeg, .png, .webp" class="form-control shadow-none" required>
                     </div>
                     <div class="col-md-12 p-0 mb-3">
                         <label class="form-label">Address</label>
-                        <textarea class="form-control shadow-none" rows="1"></textarea>
+                        <textarea name="address" class="form-control shadow-none" rows="1" required></textarea>
                     </div>
                     <div class="col-md-6 ps-0 mb-3">
                         <label class="form-label">Pin Code</label>
-                        <input type="number" class="form-control shadow-none">
+                        <input name="pincode" type="number" class="form-control shadow-none" required>
                     </div>
                     <div class="col-md-6 p-0 mb-3">
                         <label class="form-label">Date of birth</label>
-                        <input type="date" class="form-control shadow-none">
+                        <input name="dob" type="date" class="form-control shadow-none"required>
                     </div>
                     <div class="col-md-6 ps-0 mb-3">
                         <label class="form-label">Password</label>
-                        <input type="password" class="form-control shadow-none">
+                        <input name="pass" type="password" class="form-control shadow-none" required>
                     </div>
                     <div class="col-md-6 p-0 mb-3">
                         <label class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control shadow-none">
+                        <input name="cpass" type="password" class="form-control shadow-none" required>
                     </div>
                 </div>
             </div>
@@ -136,6 +163,35 @@
             </div>
         </form>
       
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="forgotModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <form id="forgot-form">
+            <div class="modal-header">
+                <h5 class="modal-title d-flex align-items-center">
+                <i class="bi bi-person-circle fs-3 me-2"></i> Forgot Password
+                </h5>
+            </div>
+            <div class="modal-body">
+                    <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+                        Note: A link will be sent to your email to reset your password!
+                    </span>
+                    <div class="mb-4">
+                    <label class="form-label">Email</label>
+                    <input type="email" name="email" required class="form-control shadow-none">
+                </div>
+                <div class="mb-2 text-end">
+                    <button type="submit" class="btn btn-dark shadow-none">SEND LINK</button>
+                    <button type="button" class="btn shadow-none p-0 me-2" data-bs-toggle="modal" data-bs-target="#loginModal" data-bs-dismiss="modal">
+                        CANCEL
+                </button>
+                </div>
+            </div>
+        </form>
     </div>
   </div>
 </div>
--- BookHotel/inc/links.php
+++ BookHotel/inc/links.php
@@ -2,3 +2,26 @@
 <link href="https://fonts.googleapis.com/css2?family=Anton&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="css/common.css">
+
+<?php 
+    session_start();
+    date_default_timezone_set("Asia/Hong_Kong");
+
+    require('admin/inc/db_config.php');
+    require('admin/inc/essentials.php');
+
+     $contact_q = "SELECT * FROM `contact_details` WHERE `sr_no`=?";
+     $settings_q = "SELECT * FROM `settings` WHERE `sr_no`=?";
+     $values = [1];
+     $contact_r = mysqli_fetch_assoc(select($contact_q,$values,'i'));
+     $settings_r = mysqli_fetch_assoc(select($settings_q,$values,'i'));
+
+     if($settings_r['shutdown']){
+        echo<<<alertbar
+            <div class='bg-danger text-center p-2 fw-bold'>
+            <i class="bi bi-exclamation-triangle-fill"></i>
+                books are temporary closed!
+            </div>
+        alertbar;
+     }
+?>
